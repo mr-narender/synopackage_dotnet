883a60796688c0de8982a22f0e1596290578febe
Szymon Bełus <sbelus@gmail.com>
2025-01-26 21:08:55 +0100

    Add support synopackage banner to main pages

--- src/Synopackage.Web/src/app/news/news.component.ts
+++ src/Synopackage.Web/src/app/news/news.component.ts
@@ -12,6 +12,7 @@ import { take } from 'rxjs/operators';
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
+  styleUrls: ['./news.component.scss']
 })
 
 export class NewsComponent implements OnInit, PaginatorDataProvider {
